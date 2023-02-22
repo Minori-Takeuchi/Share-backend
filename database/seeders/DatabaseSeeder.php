88dff6cbafe8679a4f841440c695b44738640153
Minori-Takeuchi <shato.min0716@gmeail.com>
2023-02-22 12:22:19 +0900

    secont commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CommentsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
